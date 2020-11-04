88b821db6e97019cbc7598f3df2e42d9f9b0b48f
chittaranjan23 <chittaranjanreddy2312@gmail.com>
2020-11-04 00:43:41 -0800

    Friend Cirlce 547 BFS solution

--- SolvedProblemsLeetCode/src/DFS/FriendCircles/Solution.java
+++ SolvedProblemsLeetCode/src/DFS/FriendCircles/Solution.java
@@ -3,6 +3,10 @@ package DFS.FriendCircles;
 // Stright forward solution calculate number of connected components
 //Do a dfs and marked as connected nodes as visited and Increment count
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+//DFS implementation
 class Solution {
     public int findCircleNum(int[][] M) {
         boolean[] visited = new boolean[M.length];
@@ -23,5 +27,31 @@ class Solution {
                 dfs(visited, j, M);
             }
         }
+    }
+}
+
+
+class Solution_BFS {
+    public int findCircleNum(int[][] M) {
+        boolean[] visited = new boolean[M.length];
+        int count = 0;
+        Queue<Integer> queue = new LinkedList();
+        for (int i = 0; i < M.length; i++) {
+            if (!visited[i]) {
+                queue.offer(i);
+                count++;
+                while (!queue.isEmpty()) {
+                    int index = queue.poll();
+                    visited[index] = true;
+                    count++;
+                    for (int j = 0; j < M[0].length; j++) {
+                        if (M[index][j] == 1 && !visited[j]) {
+                            queue.offer(j);
+                        }
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
